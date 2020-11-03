4f273f5dbcf0b4afb88afa6305b491ecb90297ae
meciwo <shanshan0474@gmail.com>
2020-11-03 17:09:24 +0900

    linting

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,20 +1,17 @@
 import torch
-import torchvision.transforms as transforms
 import torch.utils.data as data
 import os
-import pickle
-import numpy as np
 import nltk
 from PIL import Image
-from build_vocab import Vocabulary
-from pycocotools.coco import COCO
+import pandas as pd
 
 
 class BokeDataset(data.Dataset):
     """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+
     def __init__(self, root, vocab, transform=None):
         """Set the path for images, captions and vocabulary wrapper.
-        
+
         Args:
             root: image directory.
             vocab: vocabulary wrapper.
@@ -23,34 +20,35 @@ class BokeDataset(data.Dataset):
         self.root = root
         self.vocab = vocab
         self.transform = transform
+        self.boke = pd.read_csv("../relation.csv")
 
     def __getitem__(self, index):
         """Returns one data pair (image and caption)."""
         vocab = self.vocab
-        boke = pd.read_csv("../relation.csv")
-        caption = boke.iloc['text']
+        boke = self.boke
+        caption = boke.iloc["text"]
         path = boke.iloc["image"]
 
-        image = Image.open(os.path.join(self.root, path)).convert('RGB')
+        image = Image.open(os.path.join(self.root, path)).convert("RGB")
         if self.transform is not None:
             image = self.transform(image)
 
         # Convert caption (string) to word ids.
         tokens = nltk.tokenize.word_tokenize(str(caption).lower())
         caption = []
-        caption.append(vocab('<start>'))
+        caption.append(vocab("<start>"))
         caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
+        caption.append(vocab("<end>"))
         target = torch.Tensor(caption)
         return image, target
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.boke)
 
 
 def collate_fn(data):
     """Creates mini-batch tensors from the list of tuples (image, caption).
-    
+
     We should build custom collate_fn rather than using default collate_fn, 
     because merging caption (including padding) is not supported in default.
     Args:
@@ -74,24 +72,26 @@ def collate_fn(data):
     targets = torch.zeros(len(captions), max(lengths)).long()
     for i, cap in enumerate(captions):
         end = lengths[i]
-        targets[i, :end] = cap[:end]        
+        targets[i, :end] = cap[:end]
     return images, targets, lengths
+
 
 def get_loader(root, vocab, transform, batch_size, shuffle, num_workers):
     """Returns torch.utils.data.DataLoader for custom coco dataset."""
     # COCO caption dataset
-    boke = BokeDataset(root=root,
-                       vocab=vocab,
-                       transform=transform)
-    
+    boke = BokeDataset(root=root, vocab=vocab, transform=transform)
+
     # Data loader for COCO dataset
     # This will return (images, captions, lengths) for each iteration.
     # images: a tensor of shape (batch_size, 3, 224, 224).
     # captions: a tensor of shape (batch_size, padded_length).
-    # lengths: a list indicating valid length for each caption. length is (batch_size).
-    data_loader = torch.utils.data.DataLoader(dataset=boke, 
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              num_workers=num_workers,
-                                              collate_fn=collate_fn)
+    # lengths: a list indicating valid length for each caption.
+    # length is (batch_size).
+    data_loader = torch.utils.data.DataLoader(
+        dataset=boke,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+    )
     return data_loader
